5bd34d6fbaad246a04d2e0d3f3a0b9712cdd9570
CarlChalmers <kyleback@chalmers.se>
2023-11-08 15:58:09 +0100

    add code to car.java

--- Car.java
+++ Car.java
@@ -1,3 +1,58 @@
-public class Car {
+import java.awt.*;
+
+
+public abstract class Car {
+
+    protected int nrDoors;
+    protected double enginePower;
+    protected double currentSpeed;
+    protected Color color;
+    protected String modelName;
+
+    public int getNrDoors(){
+        return nrDoors;
+    }
+    public double getEnginePower(){
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+        return color;
+    }
+
+    public void setColor(Color clr){
+	    color = clr;
+    }
+
+    public void startEngine(){
+	    currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+	    currentSpeed = 0;
+    }
+
+
+
+    public void incrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed()  * amount;
+    }
+
+    public void decrementSpeed(double amount){
+        currentSpeed = getCurrentSpeed() * amount;
+    }
     
+    // TODO fix this method according to lab pm
+    public void gas(double amount){
+        incrementSpeed(amount);
+    }
+
+    // TODO fix this method according to lab pm
+    public void brake(double amount){
+        decrementSpeed(amount);
+    }
 }
