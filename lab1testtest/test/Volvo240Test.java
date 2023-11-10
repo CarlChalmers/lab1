ea40e711c07d5c45a149f33cc3026dcf9eeb0245
CarlKW <carl.k-wennerlof@telia.com>
2023-11-10 11:44:17 +0100

    add tests for gas volvo

--- lab1testtest/test/Volvo240Test.java
+++ lab1testtest/test/Volvo240Test.java
@@ -1,4 +1,4 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.awt.*;
@@ -13,5 +13,22 @@ public class Volvo240Test {
         assertEquals(4, volvoTest.getNrDoors() );
         assertEquals(0, volvoTest.getCurrentSpeed(),0.001);
         assertEquals(Color.black, volvoTest.getColor());
+        volvoTest.setColor(Color.red);
+        assertEquals(Color.red, volvoTest.getColor());
     }
+    @Test
+    public void gasTest() {
+        Volvo240 volvoTest = new Volvo240();
+
+        volvoTest.gas(0.5);
+        assertTrue(volvoTest.getCurrentSpeed() > 0);
+        assertTrue(volvoTest.getCurrentSpeed() <= volvoTest.getEnginePower());
+
+        double previousSpeed = volvoTest.getCurrentSpeed();
+        volvoTest.gas(-0.5);
+        assertEquals(previousSpeed, volvoTest.getCurrentSpeed(), 0.001);
+        volvoTest.gas(-10);
+        assertEquals(0,volvoTest.getCurrentSpeed(),0.001);
+    }
+
 }
